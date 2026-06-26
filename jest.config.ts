a8d9894a1325c153161bcb98c79a4287d7949663
jaydenfyi <213395523+jaydenfyi@users.noreply.github.com>
2026-06-26 13:58:00 +0800

    build: add TypeScript tooling (tsc, ts-jest)

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
